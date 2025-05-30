5cfca4bbae66b4677f89b63b0fe8c5fd7ee0b402
Nguyễn Quang Minh (NQM) <nguyenquangminh391@gmail.com>
2025-05-30 21:09:38 +0700

    refactor: update SDK and version configurations to use version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,14 +6,15 @@ plugins {
 
 android {
     namespace = "com.nqmgaming.diablito"
-    compileSdk = 35
+    compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
         applicationId = "com.nqmgaming.diablito"
-        minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+        versionCode = libs.versions.versionCode.get().toInt()
+        versionName = libs.versions.versionName.get()
+
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -33,7 +34,7 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = libs.versions.jvmTarget.get()
     }
     buildFeatures {
         compose = true
